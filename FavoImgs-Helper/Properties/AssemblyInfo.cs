1d772735386265d043b7fd5d5b97b5e122b35661
Seungho Kim <niceb5y@gmail.com>
2014-08-29 21:15:52 +0900

    version fix

--- FavoImgs-Helper/Properties/AssemblyInfo.cs
+++ FavoImgs-Helper/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // 어셈블리와 관련된 정보를 수정하려면
 // 이 특성 값을 변경하십시오.
 [assembly: AssemblyTitle("FavoImgs-Helper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("관트짤줍 프론트엔드")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FavoImgs-Helper")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © niceb5y  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 버전이 자동으로
 // 지정되도록 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
