abc9809c9bf618ce6550e81d99cf46148753eb4b
Seungho Kim <niceb5y@gmail.com>
2015-06-20 18:47:16 +0900

    add grouping, hashtag

--- FavoImgs-Helper/MainWindow.xaml.cs
+++ FavoImgs-Helper/MainWindow.xaml.cs
@@ -21,11 +21,14 @@ namespace FavoImgs_Helper
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        private bool initial;
 
         public MainWindow()
         {
+            initial = true;
             InitializeComponent();
         }
 
@@ -40,24 +43,11 @@ namespace FavoImgs_Helper
             //관트짤줍이 없는 경우
             if (!File.Exists(System.Environment.CurrentDirectory + "\\FavoImgs.exe"))
             {
-                //자동 다운로드 X
                 System.Windows.MessageBox.Show("관트짤줍이 존재하지 않습니다.\n프로그램을 다운로드받고 관트짤줍 헬퍼를 다시 실행해 주세요.");
                 //다운로드 페이지 열기
                 System.Diagnostics.Process.Start("explorer.exe", "http://azyu.tumblr.com/post/89925086759/favoimgs");
                 Environment.Exit(0);
-                /*//자동 다운로드 O
-                System.Windows.MessageBox.Show("관트짤줍이 존재하지 않습니다.\n관트짤줍을 다운로드합니다.");
-                WebClient webCL = new WebClient();
-                Utility.downloadFile(Environment.CurrentDirectory, "http://niceb5y.net/data/favoimgs/SQLite.Interop.dll", "http://niceb5y.net/data/favoimgs/FavoImgs.exe");
-                System.Windows.MessageBox.Show("관트짤줍 다운로드 완료.");
-                */
             }
-            /*관트짤줍이 알아서 하도록
-            if (!File.Exists(System.Environment.CurrentDirectory + "\\Cache.db"))
-            {
-                //Cache.db가 없으면 데이터 베이스 연결에 실패하였다며 진행되지 않으므로, 빈 파일을 생성
-                File.Create(System.Environment.CurrentDirectory + "\\Cache.db").Close();
-            }*/
         }
 
         private void btnPathReset_Click(object sender, RoutedEventArgs e)
@@ -76,18 +66,6 @@ namespace FavoImgs_Helper
             lblPath.Content = dialog.SelectedPath;
         }
 
-        private void cbList_Selected(object sender, RoutedEventArgs e)
-        {
-            //리스트 이름 텍스트박스 활성화
-            txtListName.IsEnabled = true;
-        }
-
-        private void cbList_Unselected(object sender, RoutedEventArgs e)
-        {
-            //리스트 이름 텍스트박스 비활성화
-            txtListName.IsEnabled = false;
-        }
-
         private void cbSoneone_Selected(object sender, RoutedEventArgs e)
         {
             //특정 인물 텍스트박스 활성화
@@ -103,21 +81,37 @@ namespace FavoImgs_Helper
         private void btnGet_Click(object sender, RoutedEventArgs e)
         {
             //옵션 변수 생성
+            string source = ((ComboBoxItem)cbSource.SelectedItem).Content.ToString();
             string Args = null;
             //소스 옵션
-            Args += "--source=" + ((ComboBoxItem)cbSource.SelectedItem).Content.ToString() + " ";
-            //리스트 이름 옵션
-            if (((ComboBoxItem)cbSource.SelectedItem).Content.ToString() == "list")
+            Args += "--source=" + source + " ";
+            //리스트/해시태그 이름 옵션
+            if (txtName.IsEnabled)
             {
-                if (txtListName.Text == "")
+                if (txtName.Text == "")
                 {
-                    System.Windows.MessageBox.Show("리스트 이름을 입력하세요.");
+                    System.Windows.MessageBox.Show(source + " 이름을 입력하세요.");
                     return;
                 }
                 else
                 {
-                    Args += "--slug=\"" + txtListName.Text + "\" ";
+                    if (source == "list") {
+                        Args += "--slug=\"" + txtName.Text + "\" ";
+                    }
+                    else 
+                    {
+                        Args += "--hashtag=\"#" + txtName.Text + "\" ";
+                    }
                 }
+            }
+            //그룹 옵션
+            if ((bool)cbGroup.IsChecked)
+            {
+                Args += "--group=screen_name ";
+            }
+            else
+            {
+                Args += "--group=none ";
             }
             //경로옵션
             if ((string)lblPath.Content != "")
@@ -150,6 +144,26 @@ namespace FavoImgs_Helper
             //정보창 띄우기
             InfoWindow _infoWindow = new InfoWindow();
             _infoWindow.Show();
+        }
+
+        private void cbSource_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //값 변경
+            if (((ComboBoxItem)cbSource.SelectedItem).Content.ToString() == "list" || ((ComboBoxItem)cbSource.SelectedItem).Content.ToString() == "hashtag")
+            {
+                txtName.IsEnabled = true;
+            }
+            else
+            {
+                if (initial)
+                {
+                    initial = false;
+                }
+                else
+                {
+                    txtName.IsEnabled = false;
+                }
+            }
         }
     }
 }
